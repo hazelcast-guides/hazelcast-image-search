6b6b5c0d39a97de0381f9f2bd05bb88e3ec49b4e
Randy May <randy.may@hazelcast.com>
2024-07-14 08:44:59 -0400

    rearrange image directory

--- images/make_dataset.py
+++ images/make_dataset.py
@@ -6,10 +6,7 @@ base = 'caltech-101/101_ObjectCategories'
 for category in os.listdir(base):
     category_dir = os.path.join(base, category)
     if os.path.isdir(category_dir):
-        os.mkdir(category)
         for n in range(1, 21):
             suffix = f'{n:04d}'
-            shutil.copy(
-                os.path.join(category_dir, f'image_{suffix}.jpg'),
-                os.path.join(category, f'{category}_{suffix}.jpg'))
+            shutil.copy(os.path.join(category_dir, f'image_{suffix}.jpg'), f'{category}_{suffix}.jpg')
 
